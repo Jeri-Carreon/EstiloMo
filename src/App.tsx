7abe55111b18fbfbbbb30ac1bea9ed4246c1b05f
Jeri Carreon <jericarreon51@gmail.com>
2026-04-29 15:47:02 +0800

    Initial commit, navbar

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Navbar from './Navbar';
+import Message from './Message';
+
+function App() {
+  return <div><Navbar></Navbar></div>
+  return <div><Message></Message></div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000', // your navbar color
+    },
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
